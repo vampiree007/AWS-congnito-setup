346b95894b34607b366ecb9c5bbe90ec2a1f57a6
vampiree007 <vikrantgupta.psit@gmail.com>
2021-11-30 14:48:21 +0530

    removed credentials

--- src/configs/aws.ts
+++ src/configs/aws.ts
@@ -1,5 +1,5 @@
 export const COGNITO = {
-  REGION: "us-east-2",
-  USER_POOL_ID: "us-east-2_v2nak5mao",
-  APP_CLIENT_ID: "2h0rtfsn7rg3joc93a546b2h70",
+  REGION: "",
+  USER_POOL_ID: "",
+  APP_CLIENT_ID: "",
 };
